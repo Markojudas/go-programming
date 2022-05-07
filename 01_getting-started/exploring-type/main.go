34d6cc663740bedb81ba45cd457b7f27d3574a5a
Jose <josehernandez1985@gmail.com>
2022-05-07 10:12:16 -0400

    More about Zero-Values

--- 01_getting-started/exploring-type/main.go
+++ 01_getting-started/exploring-type/main.go
@@ -6,6 +6,8 @@ var y = 42 // meaning of life :)
 var z = "Shaken, not stirred"
 var a string = `James said, "Shaken, not stirred"` //string literal; keeps spaces and new lines!
 
+var x string // DECLARED X to be TYPE string
+
 var sl string = `Here I am
 to prove you
 that this is 3 lines`
@@ -27,4 +29,11 @@ func main() {
 	// z only holds type string
 	// the variable was DECLARED with the IDENTIFIER z
 	// and of TYPE string.
+
+	// DECLARE a variable to be if a certain TYPE
+	// and then ASSIGN a VALUE of that type to the variable
+
+	fmt.Println("Printing the value of x", x, "ending") //this will print an empty string
+	// because nothing was assigned to it; defaults to zero value!
+	fmt.Printf("%T\n", x)
 }
