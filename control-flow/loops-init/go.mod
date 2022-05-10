8827f0a58f7007a2764d289a8c8de081d36b52ef
Jose <josehernandez1985@gmail.com>
2022-05-10 14:14:01 -0400

    learnt some about control flow: simple 'for' loops

---
+++ control-flow/loops-init/go.mod
@@ -0,0 +1,3 @@
+module github.com/markojudas/go-prgramming
+
+go 1.18
---
+++ control-flow/loops-init/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+/*
+	for init; condition; post {
+		...
+	}
+*/
+//no "while"
+
+func main() {
+
+	for i := 0; i <= 100; i++ {
+		fmt.Println(i)
+	}
+
+}
