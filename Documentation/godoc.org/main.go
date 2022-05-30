3f72533f0bc4d424e8a05958f57922bc3a9de5ae
Jose <josehernandez1985@gmail.com>
2022-05-30 12:39:13 -0400

    added mjmath package

---
+++ Documentation/godoc.org/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/markojudas/go-programming/Documentation/godoc.org/mjmath"
+)
+
+func main() {
+
+	fmt.Println("2 + 3 =", mjmath.Sum(2, 3))
+	fmt.Println("4 + 7 =", mjmath.Sum(4, 7))
+	fmt.Println("5 + 9 =", mjmath.Sum(5, 9))
+
+}
